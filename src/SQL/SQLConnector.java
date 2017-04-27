7af579c3206b5d770d0f128608a08bf04eda5055
Jakubinyi <jakubinyi@gmail.com>
2017-04-27 10:40:32 +0200

    SQL connector your pass

--- src/SQL/SQLConnector.java
+++ src/SQL/SQLConnector.java
@@ -14,7 +14,7 @@ public class SQLConnector {
 	public SQLConnector(){
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/lms", "root", "yourpass");
+			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/lms", "root", "yourpassword");
 			System.out.println("New SQL connection!");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
